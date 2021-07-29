01650e91b08a88b235af0b590ade17adc5b844cc
gideontech <74846017+gideontech@users.noreply.github.com>
2021-07-29 20:24:19 +0300

    Add files via upload

---
+++ FormTimeInOut.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormTimeInOut
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.EMPLOYEEID = New System.Windows.Forms.TextBox()
+        Me.btnTimeInOut = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.LabelDate = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'EMPLOYEEID
+        '
+        Me.EMPLOYEEID.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.EMPLOYEEID.Location = New System.Drawing.Point(12, 37)
+        Me.EMPLOYEEID.Multiline = True
+        Me.EMPLOYEEID.Name = "EMPLOYEEID"
+        Me.EMPLOYEEID.Size = New System.Drawing.Size(496, 45)
+        Me.EMPLOYEEID.TabIndex = 0
+        Me.EMPLOYEEID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnTimeInOut
+        '
+        Me.btnTimeInOut.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTimeInOut.Location = New System.Drawing.Point(514, 37)
+        Me.btnTimeInOut.Name = "btnTimeInOut"
+        Me.btnTimeInOut.Size = New System.Drawing.Size(251, 45)
+        Me.btnTimeInOut.TabIndex = 1
+        Me.btnTimeInOut.Text = "TIME IN / OUT"
+        Me.btnTimeInOut.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(8, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(176, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "ENTER EMPLOYEE ID"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AllowUserToResizeColumns = False
+        Me.DataGridView1.AllowUserToResizeRows = False
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.Gainsboro
+        Me.DataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.DataGridView1.ColumnHeadersHeight = 40
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 88)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(753, 336)
+        Me.DataGridView1.TabIndex = 9
+        '
+        'LabelDate
+        '
+        Me.LabelDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelDate.Location = New System.Drawing.Point(667, -1)
+        Me.LabelDate.Name = "LabelDate"
+        Me.LabelDate.Size = New System.Drawing.Size(100, 38)
+        Me.LabelDate.TabIndex = 10
+        Me.LabelDate.Text = "Label2"
+        Me.LabelDate.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 10000
+        '
+        'FormTimeInOut
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(779, 436)
+        Me.Controls.Add(Me.LabelDate)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnTimeInOut)
+        Me.Controls.Add(Me.EMPLOYEEID)
+        Me.Name = "FormTimeInOut"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TIME IN / OUT"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents EMPLOYEEID As TextBox
+    Friend WithEvents btnTimeInOut As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents LabelDate As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+End Class
--- FormTimeInOut.vb
+++ FormTimeInOut.vb
@@ -1,17 +1,49 @@
 ﻿Public Class FormTimeInOut
-    Private Sub Timer2_Tick(sender As Object, e As EventArgs)
-        ReloadAttendance()
-    End Sub
-
-    Private Sub btnTimeIn0ut_Click_1(sender As Object, e As EventArgs) Handles btnTimeIn0ut.Click
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         LabelDate.Text = DateTime.Now.ToString("yyyy/M/dd")
     End Sub
 
-    Private Sub Timer1_Tick_1(sender As Object, e As EventArgs) Handles Timer1.Tick
-        ReloadAttendance()
+    Private Sub btnTimeInOut_Click(sender As Object, e As EventArgs) Handles btnTimeInOut.Click
+        Try
+            If EMPLOYEEID.Text = "" Then
+                MessageBox.Show("Please enter Employee ID", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Else
+                reloadtxt("SELECT * FROM table_employee WHERE EMPLOYEEID='" & EMPLOYEEID.Text & "'")
+                If dt.Rows.Count > 0 Then
+                    reloadtxt("SELECT * FROM table_attendance WHERE EMPLOYEEID='" & EMPLOYEEID.Text & "' AND LOGDATE='" & LabelDate.Text & "' AND AM_STATUS='Time In' AND PM_STATUS='Time Out'")
+                    If dt.Rows.Count > 0 Then
+                        MessageBox.Show("You have already time in and timeout for today", "Already", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    Else
+                        reloadtxt("SELECT * FROM table_attendance WHERE EMPLOYEEID ='" & EMPLOYEEID.Text & "' AND LOGDATE='" & LabelDate.Text & "' AND AM_STATUS='Time In'")
+                        If dt.Rows.Count > 0 Then
+                            updatesLogged("UPDATE table_attendance SET TIMEOUT='" & TimeOfDay & "', PM_STATUS='Time Out',TOTALHOURS=TIMEOUT-TIMEIN  WHERE EMPLOYEEID='" & EMPLOYEEID.Text & "' AND LOGDATE='" & LabelDate.Text & "'")
+                            MessageBox.Show("Successfully Time Out", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        Else
+                            createLogged("INSERT INTO table_attendance(EMPLOYEEID,LOGDATE,TIMEIN,AM_STATUS)VALUES('" & EMPLOYEEID.Text & "','" & LabelDate.Text & "','" & TimeOfDay & "','Time In')")
+                            MessageBox.Show("Successfully Time In", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        End If
+                    End If
+                Else
+                    MessageBox.Show("Employeed ID not found", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+                End If
+            End If
+        Catch ex As Exception
+
+        End Try
     End Sub
 
-    Private Sub LabelDate_Click(sender As Object, e As EventArgs) Handles LabelDate.Click
+    Private Sub FormTimeInOut_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ReloadAttendance()
+    End Sub
+    Public Sub ReloadAttendance()
+        Try
+            reload("SELECT CONCAT(FIRSTNAME,' ',LASTNAME) AS FULLNAME,LOGDATE,TIMEIN,AM_STATUS,TIMEOUT,PM_STATUS,TOTALHOURS FROM table_attendance INNER JOIN table_employee ON table_attendance.EMPLOYEEID=table_employee.EMPLOYEEID", DataGridView1)
+        Catch ex As Exception
 
+        End Try
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        ReloadAttendance()
     End Sub
 End Class
